e651a6694135c24741f0eeafadca2e4296fc019a
systemshock89 <666qqq77>
2024-05-03 17:41:20 +0300

    init

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Wave hover effect
+
+[See demo](https://systemshock89.github.io/demo-wave-hover-effect/)
+
+## Used features
+* only css
+* use `:has`
+* `tabindex` for click to active element
+* vite
+
+## How to use
+
+* `git clone`
+* `npm install`
+* `npm run dev`
---
+++ vite.config.js
@@ -0,0 +1,45 @@
+import autoprefixer from 'autoprefixer';
+import viteImagemin from 'vite-plugin-imagemin'
+
+export default {
+    server: {
+        open: true,
+    },
+    css: {
+        postcss: {
+            plugins: [autoprefixer],
+        },
+    },
+    plugins: [
+        viteImagemin({
+            gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false,
+            },
+            optipng: {
+                optimizationLevel: 7,
+            },
+            mozjpeg: {
+                quality: 75,
+            },
+            pngquant: {
+                quality: [0.8, 0.9],
+                speed: 4,
+            },
+            svgo: {
+                plugins: [
+                    {
+                        name: 'removeViewBox',
+                    },
+                    {
+                        name: 'removeEmptyAttrs',
+                        active: false,
+                    },
+                ],
+            },
+            webp: {
+                quality: 90,
+            }
+        }),
+    ],
+}
